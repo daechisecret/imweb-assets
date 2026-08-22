942c2df8198e67f3d11813736e1390e27428db7e
daechisecret <shbaeg93@gmail.com>
2026-08-22 16:30:25 +0900

    꾸미기 파일 새로 만듦

--- header.js
+++ header.js
@@ -106,7 +106,7 @@
 
 (function () {
   var BASE = 'https://daechisecret.github.io/imweb-assets/';
-  var V = '70032ff1';
+  var V = '72c5f7b6';
   var done = false;
   function go() {
     if (done || !document.querySelector('.shop-content.mypage')) return;
